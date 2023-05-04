4e3f5fff22f9fd97d2990cccabb0a9720c89b581
Nguyen Thuan An <thuanan7@gmail.com>
2023-05-04 21:02:55 +0700

    Bai 11 - pagination va web dong ver tu lam

--- js/21810201.js
+++ js/21810201.js
@@ -1,10 +1,15 @@
 const apiURL = "https://web1-api.vercel.app/api";
 
-async function getData(request, templateId, viewId) {
+async function getData(request, templateId, viewId, props={}) {
 	const response = await fetch(`${apiURL}/${request}`);
 	const jsonData = await response.json();
+	
+	const context = {};
+	context.data = jsonData;
+	Object.assign(context.data, props);
+	
 	var source = document.getElementById(templateId).innerHTML;
 	var template = Handlebars.compile(source);
-	var html = template({ data: jsonData });
+	var html = template(context);
 	document.getElementById(viewId).innerHTML = html;
 }
